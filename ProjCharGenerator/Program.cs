44312b0819ba8d63b1c1ecbe3a31444ca0618a41
ddrzxc <147187316+ddrzxc@users.noreply.github.com>
2024-06-24 16:24:42 +0300

    Добавлен BigrGenerator

--- ProjCharGenerator/Program.cs
+++ ProjCharGenerator/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace generator
 {
@@ -17,6 +19,51 @@ namespace generator
         public char getSym() 
         {
            return data[random.Next(0, size)]; 
+        }
+    }
+    class BigrGenerator
+    {
+        private Dictionary<string, int> map;
+        private int max;
+        public BigrGenerator()
+        {
+            map = new Dictionary<string, int>();
+            max = 0;
+        }
+        public void Load(string fileName)
+        {
+            List<string> lines = File.ReadAllLines(fileName).ToList();
+            Dictionary<string, int> newmap = new Dictionary<string, int>();
+            foreach (string line in lines)
+            {
+                string[] words = line.Split('\t', StringSplitOptions.TrimEntries);
+                max += Convert.ToInt32(words[2]);
+                newmap.Add(words[1], max);
+            }
+            Load(newmap);
+        }
+        public void Load(Dictionary<string, int> load)
+        {
+            map = load;
+        }
+        public string GetSym()
+        {
+            Random rand = new Random();
+            long num = rand.NextInt64(max);
+            foreach (var pair in map)
+            {
+                if (num <= pair.Value) return pair.Key;
+            }
+            return map.Keys.ToList().Last();
+        }
+        public string GetString(int count)
+        {
+            string res = "";
+            for (int i = 0; i < count; i++)
+            {
+                res += GetSym();
+            }
+            return res;
         }
     }
     class Program
@@ -38,7 +85,11 @@ namespace generator
             {
                  Console.WriteLine("{0} - {1}",entry.Key,entry.Value/1000.0); 
             }
-            
+
+            BigrGenerator bgen = new BigrGenerator();
+            bgen.Load("bigrams.txt");
+            Console.WriteLine(bgen.GetString(10));
+            File.WriteAllText("gen-1.txt", bgen.GetString(1000));
         }
     }
 }
